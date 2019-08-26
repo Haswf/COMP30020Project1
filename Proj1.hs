a04fa4c0658328286cc57266bdd2b53678f5b3ef
Haswell <shuyangf@student.unimelb.edu.au>
2019-08-26 21:25:42 +1000

    Implemented compareFeedBack, initialGuess function and Signal type.

--- Proj1.hs
+++ Proj1.hs
@@ -1,17 +1,17 @@
-module Proj1 (Pitch, toPitch, removeJust, compareNote, compareOctave, Octave, Note, feedback, GameState) where
+module Proj1 (Pitch, toPitch, compareNote, compareOctave, Octave, Note, feedback, GameState) where
 import Data.Maybe
 import Data.List
 
 data Octave = One | Two | Three deriving (Read, Eq, Enum, Bounded)
 data Note = A | B | C | D | E | F | G deriving (Show, Read, Eq, Enum, Bounded)
 data Pitch = Pitch Note Octave deriving (Read, Eq)
-data GameState = GameState { attempted ::[[Pitch]]
-                           , remaining :: [[Pitch]]
+data GameState = GameState { remaining :: [SolutionSpace]
+                           , prevFeedback :: (Int, Int, Int)
+                           , prevGuess :: [Pitch]
 } deriving Show
 
--- A collection of all possible pitchs
-type CombinationByNote = [[Pitch]]
-
+-- A collection of all possible pitchs groupby note
+type SolutionSpace = [[Pitch]]
 
 -- Show instance for Octave. Convert ocatve to String
 instance Show Octave where
@@ -50,7 +50,6 @@ toPitch str
     where octave = readOctave (str !! 1)
           note = readNote (str !! 0)
 
-
 -- return note of a pitch
 getNote :: Pitch -> Note
 getNote (Pitch n _) = n
@@ -58,10 +57,6 @@ getNote (Pitch n _) = n
 -- return octave of a pitch
 getOctave :: Pitch -> Octave
 getOctave (Pitch _ o) = o
-
--- convert a list of Maybe Pitch to Pitch
-removeJust :: [Maybe Pitch] -> [Pitch]
-removeJust = map fromJust
 
 -- function to compare if two pitchs have the same note but different octave
 compareNote :: Pitch -> Pitch -> Bool
@@ -75,6 +70,7 @@ compareOctave p1 p2
     | (getNote p1 /= getNote p2) && (getOctave p1 == getOctave p2) = True
     | otherwise = False
 
+-- Providing feedback on the number of matching pitch\notes\ocataves.
 feedback :: [Pitch] -> [Pitch] -> (Int, Int, Int)
 feedback guess target = (length intersection, 
                          length (intersectBy compareNote uniqueTarget uniqueGuess),
@@ -83,12 +79,54 @@ feedback guess target = (length intersection,
           uniqueTarget = target \\ intersection
           uniqueGuess = guess \\ intersection
 
---
+-- Read pitch(s) from a list of String
+readInputFromList :: [String] -> [Pitch]
+-- Filter out Nothing in the list
+readInputFromList input = map (fromJust . toPitch) input
 
-generateInput :: [String] -> [Pitch]
-generateInput input = removeJust (map toPitch input)
-
-genComByNote :: CombinationByNote
-genComByNote = groupBy compareNote [Pitch note octave |
-                                         note <- [A .. G], 
+-- Initialise a solution space which contains all possible pitches
+initialSolutionSpace :: SolutionSpace
+initialSolutionSpace = groupBy compareNote [Pitch note octave |
+                                         note <- [A .. G],              
                                          octave <- [One .. Three]]
+
+
+-- Create inisital guess 
+initialGuess :: ([Pitch],GameState)
+initialGuess = (guess, gamestate)
+                where fullCombination = [initialSolutionSpace, initialSolutionSpace, initialSolutionSpace]
+                      guess = (map nextPitch fullCombination) -- for each SolutionSpace, extract next pitch
+                      gamestate = GameState {remaining=fullCombination, prevFeedback=(0,0,0), prevGuess=guess}
+
+-- Extract next element from a SolutionSpace
+nextPitch :: SolutionSpace -> Pitch
+nextPitch comb = head (head comb)
+
+-- Diff function to compare two guesses. 
+-- Can be used to find which card results in change in feedback.
+diff :: [Pitch] -> [Pitch] -> [Pitch]
+diff prev this = this \\ prev
+
+
+-- Signal is defined to manipulate game state
+data Signal = KeepPitch -- found matching pitch
+    | KeepNote          -- found matching note, change octace
+    | KeepOctave        -- found matching octave, change note
+    | Discard           -- No luck
+    deriving (Show, Eq)
+
+
+-- compareFeedback compares feedback of two guesses.
+-- send a signal on how to change game state.
+compareFeedback :: (Int, Int, Int) -> (Int, Int, Int) -> Signal
+compareFeedback (prevMatch, prevNotes, prevOctaves) (thisMatch, thisNotes, thisOctaves) 
+    | thisMatch > prevMatch = KeepPitch
+    | thisNotes > prevNotes = KeepNote
+    | thisOctaves > prevOctaves = KeepOctave
+    | otherwise = Discard
+
+-- TODO: To be implemented
+-- updateGameState :: ([Pitch], GameState) -> Signal -> [Pitch]
+-- updateGameState (guess, old) signal
+--     | signal == KeepPitch = changeElem
+--         where changeElem = diff guess (prevGuess old)
