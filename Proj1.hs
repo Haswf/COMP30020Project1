63252af24de3273153b6e129bd843aa75445a128
Haswell <shuyangf@student.unimelb.edu.au>
2019-09-02 01:04:52 +1000

    Rework of findRight, findWrong with case and guards

--- Proj1.hs
+++ Proj1.hs
@@ -7,9 +7,9 @@ import Data.List
 type Feedback = (Int, Int, Int, Int, Int)
 
 data GameState = GameState{
-                        wrongLeft :: [Card],
-                        rightLeft :: [Card],
-                        wrongGuess :: [Card],
+                        wrongLeft :: [Card], -- cards that haven't been test for incorrect
+                        rightLeft :: [Card], -- cards that haven't been test for corect
+                        wrongGuess :: [Card], -- A set of cards which have been tested to be incorrect
                         correctGuess :: [Card]
                         } deriving (Show)
 
@@ -35,14 +35,6 @@ getRank (Card _ r) = r
 getSuit :: Card -> Suit
 getSuit (Card s _) = s
 
--- Parse guess from input
-getGuess :: ([Card], GameState) -> [Card]
-getGuess (guess, _) = guess
-
--- Parse GameState from input
-getGameState :: ([Card], GameState) -> GameState
-getGameState (_, gs) = gs
-
 initialGuess :: Int -> ([Card], GameState)
 initialGuess num = (guess, gamestate)
     where guess = take num allCards
@@ -54,20 +46,23 @@ initialGuess num = (guess, gamestate)
           allCards = [minBound..maxBound] :: [Card]
 
 findWrong :: ([Card], GameState) -> Feedback -> ([Card], GameState)
-findWrong (thisGuess, GameState wrongLeft rightleft wrongguess correctguess) (exactMatch, _, _,_,_)
-    | exactMatch == 0 = let nextStageGameState =  GameState { wrongLeft=[], -- found enough wrong cards, no longer need to keep cards haven't been tested to be wrong
-                                                              rightLeft=rightleft \\ thisGuess, -- no need to change right left
-                                                              wrongGuess=thisGuess, -- store wrong cards in wrong guess
-                                                              correctGuess=[]}
-                        in (thisGuess, nextStageGameState)
-    | otherwise = let newGuess = tail thisGuess ++ [head wrongLeft]  -- generate next guess to find wrong cards
-                      newGameState = GameState { wrongLeft=tail wrongLeft,
-                                                 rightLeft=rightleft, -- no need to change right left
-                                                 wrongGuess=wrongguess,
-                                                 correctGuess=correctguess}
-                     in (newGuess, newGameState)
-
-
+findWrong (thisGuess, GameState wrongLeft rightleft wrongguess correctguess) (exactMatch, _, _,_,_) = 
+    let newWrongLeft 
+            | exactMatch == 0 = []
+            | otherwise = tail wrongLeft
+        newRightLeft 
+            | exactMatch == 0 = rightleft \\ thisGuess
+            | otherwise = rightleft
+        newWrongGuess 
+            | exactMatch == 0 = thisGuess
+            | otherwise = wrongguess
+        newGameState = GameState {  wrongLeft=newWrongLeft, -- found enough wrong cards, no longer need to keep cards haven't been tested to be wrong
+                                    rightLeft=newRightLeft, -- no need to change right left
+                                    wrongGuess=newWrongGuess, -- store wrong cards in wrong guess
+                                    correctGuess=[]}
+        newGuess = tail thisGuess ++ [head wrongLeft]
+    in (newGuess, newGameState)
+        
 nextGuess :: ([Card],GameState) -> Feedback -> ([Card],GameState)
 nextGuess (thisGuess, thisGameState) fb
     -- if we haven't found length card distinct cards that's not in target
@@ -76,30 +71,16 @@ nextGuess (thisGuess, thisGameState) fb
     | otherwise = findCorrect (thisGuess, thisGameState) fb
     where answerLen = length thisGuess
           
-
 findCorrect :: ([Card],GameState) -> Feedback -> ([Card],GameState)
-findCorrect (thisGuess, GameState wrongLeft rightleft wrongguess correctguess) (exactMatch, _, _,_,_)
-    | exactMatch == 0 = let newGuess = twoWrongCards ++ [head rightleft]  -- generate next guess to find wrong cards
-                            newGameState = GameState { wrongLeft=[],
-                                                       rightLeft=tail rightleft, -- no need to change right left
-                                                       wrongGuess=wrongguess,
-                                                      correctGuess=correctguess}
-                        in (newGuess, newGameState)
-    | otherwise = let newGuess = twoWrongCards ++ [head rightleft]  -- generate next guess to find wrong cards
-                      newGameState = GameState { wrongLeft=[],
-                               rightLeft=tail rightleft, -- no need to change right left
-                               wrongGuess=wrongguess,
-                               correctGuess=correctguess++[last thisGuess]}
-                        in (newGuess, newGameState)
+findCorrect (thisGuess, GameState wrongLeft rightleft wrongguess correctguess) (exactMatch, _, _,_,_) = 
+    let newGuess = twoWrongCards ++ [head rightleft]
+        newCorrectGuess = case exactMatch of 0 -> correctguess
+                                             1 -> correctguess++[last thisGuess]
+        newGameState = GameState {  wrongLeft=[],
+                                    rightLeft=tail rightleft, -- no need to change right left
+                                    wrongGuess=wrongguess,
+                                    correctGuess=newCorrectGuess}
+
+    in (newGuess, newGameState)
     where ansLength = length thisGuess
           twoWrongCards = take (ansLength-1) wrongguess
-
-
--- getWrong :: GameState -> [Card]
--- getWrong (GameState _  wrong  _) = wrong
-
--- getCorrect :: GameState -> [Card]
--- getCorrect (GameState _  _  correct) = correct
-
--- getLeft :: GameState -> [Card]
--- getLeft (GameState left  _  _) = left
