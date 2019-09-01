923aeb21ceee313b6b11ebe3cf0ab1416948a067
Haswell <shuyangf@student.unimelb.edu.au>
2019-09-02 00:31:31 +1000

    First working implementation of nextGuess despite it has low quality

--- Proj1.hs
+++ Proj1.hs
@@ -1,10 +1,14 @@
+
+module Proj1 (feedback, initialGuess, nextGuess, GameState) where
+    
 import Card
 import Data.List
 
 type Feedback = (Int, Int, Int, Int, Int)
 
 data GameState = GameState{
-                        remaining :: [Card],
+                        wrongLeft :: [Card],
+                        rightLeft :: [Card],
                         wrongGuess :: [Card],
                         correctGuess :: [Card]
                         } deriving (Show)
@@ -31,73 +35,71 @@ getRank (Card _ r) = r
 getSuit :: Card -> Suit
 getSuit (Card s _) = s
 
+-- Parse guess from input
 getGuess :: ([Card], GameState) -> [Card]
 getGuess (guess, _) = guess
 
+-- Parse GameState from input
 getGameState :: ([Card], GameState) -> GameState
 getGameState (_, gs) = gs
 
 initialGuess :: Int -> ([Card], GameState)
 initialGuess num = (guess, gamestate)
     where guess = take num allCards
-          gamestate = GameState { remaining=allCards \\ guess, wrongGuess=[], correctGuess=[]}
+          gamestate = GameState { wrongLeft=allCards \\ guess, 
+                                  rightLeft=allCards, 
+                                  wrongGuess=[], 
+                                  correctGuess=[]}
+          -- all cards in card type
           allCards = [minBound..maxBound] :: [Card]
 
 findWrong :: ([Card], GameState) -> Feedback -> ([Card], GameState)
-findWrong (thisGuess, GameState left wrong correct) (exactMatch, _, _,_,_)
-    | exactMatch == 0 = (thisGuess,  GameState { remaining=left,
-                                                 wrongGuess=thisGuess,
-                                                 correctGuess=[]})
-    | otherwise = (newGuess, newGameState)
-    where newGuess = tail thisGuess ++ [head left]
-          newGameState = GameState { remaining=tail left,
-                                     wrongGuess=wrong,
-                                     correctGuess=correct}
-          
+findWrong (thisGuess, GameState wrongLeft rightleft wrongguess correctguess) (exactMatch, _, _,_,_)
+    | exactMatch == 0 = let nextStageGameState =  GameState { wrongLeft=[], -- found enough wrong cards, no longer need to keep cards haven't been tested to be wrong
+                                                              rightLeft=rightleft \\ thisGuess, -- no need to change right left
+                                                              wrongGuess=thisGuess, -- store wrong cards in wrong guess
+                                                              correctGuess=[]}
+                        in (thisGuess, nextStageGameState)
+    | otherwise = let newGuess = tail thisGuess ++ [head wrongLeft]  -- generate next guess to find wrong cards
+                      newGameState = GameState { wrongLeft=tail wrongLeft,
+                                                 rightLeft=rightleft, -- no need to change right left
+                                                 wrongGuess=wrongguess,
+                                                 correctGuess=correctguess}
+                     in (newGuess, newGameState)
 
-testFindWrong :: [Card] -> ([Card], GameState)
-testFindWrong answer = findWrong (guess, gamestate) (feedback answer guess)
-    where retval = initialGuess (length answer)
-          guess = getGuess retval
-          gamestate = getGameState retval
 
 nextGuess :: ([Card],GameState) -> Feedback -> ([Card],GameState)
 nextGuess (thisGuess, thisGameState) fb
+    -- if we haven't found length card distinct cards that's not in target
+    | length (correctGuess thisGameState) == answerLen = ((correctGuess thisGameState), thisGameState)
     | length (wrongGuess thisGameState) /= answerLen = findWrong (thisGuess, thisGameState) fb
-    | otherwise = initialGuess 3
+    | otherwise = findCorrect (thisGuess, thisGameState) fb
     where answerLen = length thisGuess
+          
 
-getWrong :: GameState -> [Card]
-getWrong (GameState _  wrong  _) = wrong
+findCorrect :: ([Card],GameState) -> Feedback -> ([Card],GameState)
+findCorrect (thisGuess, GameState wrongLeft rightleft wrongguess correctguess) (exactMatch, _, _,_,_)
+    | exactMatch == 0 = let newGuess = twoWrongCards ++ [head rightleft]  -- generate next guess to find wrong cards
+                            newGameState = GameState { wrongLeft=[],
+                                                       rightLeft=tail rightleft, -- no need to change right left
+                                                       wrongGuess=wrongguess,
+                                                      correctGuess=correctguess}
+                        in (newGuess, newGameState)
+    | otherwise = let newGuess = twoWrongCards ++ [head rightleft]  -- generate next guess to find wrong cards
+                      newGameState = GameState { wrongLeft=[],
+                               rightLeft=tail rightleft, -- no need to change right left
+                               wrongGuess=wrongguess,
+                               correctGuess=correctguess++[last thisGuess]}
+                        in (newGuess, newGameState)
+    where ansLength = length thisGuess
+          twoWrongCards = take (ansLength-1) wrongguess
 
-getCorrect :: GameState -> [Card]
-getCorrect (GameState _  _  correct) = correct
 
-getLeft :: GameState -> [Card]
-getLeft (GameState left  _  _) = left
+-- getWrong :: GameState -> [Card]
+-- getWrong (GameState _  wrong  _) = wrong
 
--- popNext :: GameState -> (Card, GameState)
--- popNext (GameState left wrongguess correctguess) = (nextCard, newGameState)
---     where nextCard = head left
---           newGameState = GameState { remaining=delete nextCard left,
---                                     wrongGuess=wrongguess,
---                                     correctGuess=correctguess}
+-- getCorrect :: GameState -> [Card]
+-- getCorrect (GameState _  _  correct) = correct
 
-
-----------------------------------------------------------------------------
--- brute force approach by computing all combinations beforehand(Discarded)
-----------------------------------------------------------------------------
--- subsequencesOfSize :: Int -> [a] -> [[a]]
--- subsequencesOfSize n xs = let l = length xs
---                           in if n>l then [] else subsequencesBySize xs !! (l-n)
-
--- subsequencesBySize :: [t] -> [[[t]]]                          
--- subsequencesBySize [] = [[[]]]
--- subsequencesBySize (x:xs) = let next = subsequencesBySize xs in zipWith (++) ([]:next) (map (map (x:)) next ++ [[]])
--- initialGuess :: Int -> ([Card],GameState)
-
--- initialGuess num = (guess, gamestate)
---     where guess = head (combinations gamestate)
---           gamestate = GameState { combinations= subsequencesOfSize num ([minBound..maxBound] :: [Card]),
---                                   prevFeedback = (0, 0, 0, 0, 0),
---                                   prevGuess=[]}
+-- getLeft :: GameState -> [Card]
+-- getLeft (GameState left  _  _) = left
